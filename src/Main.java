50f43cedd5ca401e909d57d84fffbf277e6d8aea
Arcedii <91275440+Arcedii@users.noreply.github.com>
2023-05-27 14:03:51 +0300

    second project

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int [] numbers = {23, 11, 25, 5, 23, 11, 89};
+        for(int i = 0; i < numbers.length; i++) {
+            int j = 0;
+            int g = 0;
+            g++;
+            int sum = j + numbers[g];
+            System.out.println(sum);
+        }
+
+    }
+}
